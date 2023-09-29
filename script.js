b9797342ee388c789f2acfa991365e83a40ed3da
4ymaneee <nalmlwmyat4@gmail.com>
2023-09-29 20:51:06 +0100

    Add function Create and Delete

---
+++ script.js
@@ -0,0 +1,32 @@
+let add = document.getElementById("add");
+let enterTask = document.getElementById("entertask");
+let tasks = document.getElementById("tasks");
+
+//validate input field
+function addButton() {
+    if (enterTask.value.length == 0) {
+        alert("please Enter a Task");
+    } else {
+        // create a task with click
+        tasks.innerHTML += `
+            <div id="task">
+                <span id="taskcontent">${enterTask.value}</span>
+                <button class="deletetask"><i class="fa-regular fa-trash-can"></i></button>
+            </div>
+        `;
+        // delete a task
+        let deleteTasks = document.getElementsByClassName('deletetask');
+        for (let i = 0; i < deleteTasks.length; i++) {
+            deleteTasks[i].onclick = function () {
+                this.parentNode.remove();
+            };
+        }
+    }
+}
+
+
+
+
+
+// Use getElementsByClassName to select elements by class
+
